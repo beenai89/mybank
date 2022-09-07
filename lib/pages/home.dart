9566a4a97f4c9c936af07d085f3211cabeb7a9fa
Abdullahi Ahmad <dynastyitsp@gmail.com>
2022-09-07 20:40:25 +0100

    Working on Home page

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -26,7 +26,7 @@ class HomePage extends StatelessWidget {
                         child: Container(
                           height: 100,
                           width: 100,
-                          color: Colors.red,
+                          child: Image.asset("assets/logo.png"),
                         ),
                       ),
                     ),
@@ -42,38 +42,203 @@ class HomePage extends StatelessWidget {
                           Expanded(
                             child: Container(
                               decoration: const BoxDecoration(
-                                  color: Colors.black12,
+                                  color: Colors.blueGrey,
                                   borderRadius: BorderRadius.only(
                                       topLeft: Radius.circular(20),
                                       bottomLeft: Radius.circular(20))),
                               width: 100,
+                              child: Padding(
+                                padding:
+                                    const EdgeInsets.symmetric(vertical: 20.0),
+                                child: Row(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    const Icon(
+                                      Icons.directions_walk_sharp,
+                                      size: 28,
+                                      color: Colors.white,
+                                    ),
+                                    Column(
+                                      crossAxisAlignment:
+                                          CrossAxisAlignment.start,
+                                      children: const [
+                                        Text(
+                                          "On the go",
+                                          style: TextStyle(color: Colors.white),
+                                        ),
+                                        SizedBox(height: 8),
+                                        Text(
+                                          "Quick cash,",
+                                          style: TextStyle(color: Colors.white),
+                                        ),
+                                      ],
+                                    )
+                                  ],
+                                ),
+                              ),
                             ),
                           ),
                           const SizedBox(height: 8),
                           Expanded(
                             child: Container(
                               decoration: const BoxDecoration(
-                                  color: Colors.black12,
+                                  color: Colors.blueGrey,
                                   borderRadius: BorderRadius.only(
                                       topLeft: Radius.circular(20),
                                       bottomLeft: Radius.circular(20))),
                               width: 100,
+                              child: Padding(
+                                padding:
+                                    const EdgeInsets.symmetric(vertical: 20.0),
+                                child: Row(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    const Icon(
+                                      Icons.food_bank_outlined,
+                                      size: 28,
+                                      color: Colors.white,
+                                    ),
+                                    Column(
+                                      crossAxisAlignment:
+                                          CrossAxisAlignment.start,
+                                      children: const [
+                                        Text(
+                                          "On the go",
+                                          style: TextStyle(color: Colors.white),
+                                        ),
+                                        SizedBox(height: 8),
+                                        Text(
+                                          "Quick cash,",
+                                          style: TextStyle(color: Colors.white),
+                                        ),
+                                      ],
+                                    )
+                                  ],
+                                ),
+                              ),
                             ),
                           ),
                           const SizedBox(height: 8),
                           Expanded(
                             child: Container(
                               decoration: const BoxDecoration(
-                                  color: Colors.black12,
+                                  color: Colors.blueGrey,
                                   borderRadius: BorderRadius.only(
                                       topLeft: Radius.circular(20),
                                       bottomLeft: Radius.circular(20))),
                               width: 100,
+                              child: Padding(
+                                padding:
+                                    const EdgeInsets.symmetric(vertical: 20.0),
+                                child: Row(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    const Icon(
+                                      Icons.card_travel_outlined,
+                                      size: 28,
+                                      color: Colors.white,
+                                    ),
+                                    Column(
+                                      crossAxisAlignment:
+                                          CrossAxisAlignment.start,
+                                      children: const [
+                                        Text(
+                                          "On the go",
+                                          style: TextStyle(color: Colors.white),
+                                        ),
+                                        SizedBox(height: 8),
+                                        Text(
+                                          "Quick cash,",
+                                          style: TextStyle(color: Colors.white),
+                                        ),
+                                      ],
+                                    )
+                                  ],
+                                ),
+                              ),
                             ),
                           ),
                         ],
                       ),
                     ),
+                  ],
+                ),
+              ),
+              const SizedBox(height: 80),
+              Expanded(
+                flex: 2,
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 30.0),
+                  child: GridView.builder(
+                      itemCount: 6,
+                      gridDelegate:
+                          const SliverGridDelegateWithFixedCrossAxisCount(
+                              crossAxisCount: 3),
+                      itemBuilder: (context, index) {
+                        return Column(
+                          children: [
+                            Container(
+                              height: 50,
+                              width: 50,
+                              decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(60),
+                                color: Colors.blueGrey,
+                              ),
+                              child: const Icon(Icons.file_copy_rounded,
+                                  color: Colors.white),
+                            ),
+                            const Text(
+                              "Quick",
+                              style: TextStyle(
+                                  color: Colors.blueGrey,
+                                  fontWeight: FontWeight.bold),
+                            ),
+                            const Text(
+                              "Balance",
+                              style: TextStyle(
+                                  color: Colors.blueGrey,
+                                  fontWeight: FontWeight.bold),
+                            ),
+                          ],
+                        );
+                      }),
+                ),
+              ),
+              Expanded(
+                child: Stack(
+                  children: [
+                    Positioned(
+                        // top: 10,
+                        bottom: 0,
+                        child: Row(
+                          children: [
+                            Container(
+                              alignment: Alignment.center,
+                              height: 50,
+                              width: 200,
+                              decoration:
+                                  const BoxDecoration(color: Colors.red),
+                              child: const Text(
+                                "Sign in VSPay",
+                                style: TextStyle(
+                                    color: Colors.white, fontSize: 20),
+                              ),
+                            ),
+                            const SizedBox(width: 8),
+                            Container(
+                              alignment: Alignment.center,
+                              height: 50,
+                              width: 200,
+                              decoration:
+                                  const BoxDecoration(color: Colors.red),
+                              child: const Text(
+                                "Sign in VSPay",
+                                style: TextStyle(
+                                    color: Colors.white, fontSize: 20),
+                              ),
+                            ),
+                          ],
+                        ))
                   ],
                 ),
               )
